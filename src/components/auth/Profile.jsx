3f88ed2d0d873a9c19305390a5fa6b97409ec996
Felix <56971417+Zerthz@users.noreply.github.com>
2023-01-21 00:17:04 +0100

    Revert "Lock upload/download behind admin"
    This reverts commit 364a26b5a40c5280740fcb0e025067293f7df896.

--- src/components/auth/Profile.jsx
+++ src/components/auth/Profile.jsx
@@ -73,23 +73,20 @@ const Profile = () => {
                     </Typography>
                     <Divider variant="middle" />
                     <Box sx={{ display: 'flex', justifyContent: 'center', gap: '1em', flexDirection: { xs: 'column', md: 'row' } }}>
-                        {currentUser.admin && <>
-                            <Button
-                                variant="contained"
-                                size="large"
-                                endIcon={<CloudUploadOutlined />}
-                                onClick={handleUpload}
-                            >Upload</Button>
-                            <Button
-                                variant="outlined"
-                                color="primary"
-                                size="large"
-                                endIcon={<CloudDownloadOutlined />}
-                                onClick={handleDownload}
-                                disabled
-                            >Download</Button>
-                        </>
-                        }
+                        <Button
+                            variant="contained"
+                            size="large"
+                            endIcon={<CloudUploadOutlined />}
+                            onClick={handleUpload}
+                        >Upload</Button>
+                        <Button
+                            variant="outlined"
+                            color="primary"
+                            size="large"
+                            endIcon={<CloudDownloadOutlined />}
+                            onClick={handleDownload}
+                            disabled
+                        >Download</Button>
                         <Button
                             variant="outlined"
                             color="secondary"
